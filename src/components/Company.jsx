4348e4b06a7d75e208390dc28461fd4f76fce951
Luigi160397 <luigi.difraia.97@gmail.com>
2023-04-06 16:16:37 +0200

    esercizio4

--- src/components/Company.jsx
+++ src/components/Company.jsx
@@ -1,31 +1,28 @@
-import { Row, Col, Button } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
-const Job = ({ data }) => {
+const Company = ({ company, index }) => {
   const dispatch = useDispatch();
 
   return (
     <Row className="mx-0 mt-3 p-3 justify-content-between" style={{ border: "1px solid #00000033", borderRadius: 4 }}>
       <Col xs={3}>
-        <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+        <Link to={`/${company.company_name}`}>{company.company_name}</Link>
       </Col>
-      <Col xs={6}>
-        <a href={data.url} target="_blank" rel="noreferrer">
-          {data.title}
-        </a>
-      </Col>
+
       <Col xs={1}>
         <Button
-          variant="primary"
+          variant="danger"
           onClick={() => {
-            dispatch({ type: "ADD_TO_FAVOURITE", payload: data });
+            dispatch({ type: "DELETE_FROM_FAVOURITE", payload: index });
           }}
         >
-          🤍
+          🗑️
         </Button>
       </Col>
     </Row>
   );
 };
-export default Job;
+
+export default Company;
